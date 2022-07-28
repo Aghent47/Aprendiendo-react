60a6ab64578be213ce4d9b31e735ca52ad50abbd
Neider Hernández M <neider.hernandez@invemar.org.co>
2022-07-28 15:26:45 -0500

    useState - Hook

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,14 +1,22 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
 
-const handleAdd = (event) => {
-  console.log(event);
-};
+
 
 export const CounterApp = ({ value }) => {
+
+    const [ counter, setCounter ] = useState( value );
+
+    const handleAdd = (event) => {
+        // console.log(event);
+        setCounter(counter + 1); 
+        // setCounter((c) => c + 1);
+      };
+
   return (
     <>
       <h1>Counter App</h1>
-      <h2> {value} </h2>
+      <h2> { counter } </h2>
       <button onClick={handleAdd} type="button" className="btn btn-outline-info"> +1 </button> 
     </>
   );
