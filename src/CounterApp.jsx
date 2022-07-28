223adbcfbbb0dd09acccd2b9184b9f20a714f045
Neider Hernández M <neider.hernandez@invemar.org.co>
2022-07-28 16:15:11 -0500

    Counter App Funcional al 100%

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,23 +1,26 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
 
-
-
 export const CounterApp = ({ value }) => {
 
     const [ counter, setCounter ] = useState( value );
 
     const handleAdd = (event) => {
-        // console.log(event);
-        setCounter(counter + 1); 
+        setCounter(counter + 1);
         // setCounter((c) => c + 1);
       };
+
+    const handleSubtract = (event) => setCounter(counter - 1);
+    const handleReset = (event) => setCounter(value);
+
 
   return (
     <>
       <h1>Counter App</h1>
       <h2> { counter } </h2>
-      <button onClick={handleAdd} type="button" className="btn btn-outline-info"> +1 </button> 
+      <button onClick={ handleAdd } type="button" className="btn btn-outline-info"> +1 </button> 
+      <button onClick={ handleSubtract } type="button" className="btn btn-outline-info"> -1 </button> 
+      <button onClick={ handleReset } type="button" className="btn btn-outline-danger"> Reset </button> 
     </>
   );
 };
@@ -25,3 +28,4 @@ export const CounterApp = ({ value }) => {
 CounterApp.propTypes = {
   value: PropTypes.number.isRequired,
 };
+q
