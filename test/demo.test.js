795f7146786c189af301ecf81bd38a027b89518b
Neider Hernández M <neider.hernandez@invemar.org.co>
2022-07-29 10:47:49 -0500

    Configurand Archivos para Pruebas con Jest - Expect - toBe

---
+++ test/demo.test.js
@@ -0,0 +1,13 @@
+describe("Pruebas en <DemoComponent>", () => {
+  test("Primera Prueba", () => {
+    //1. Inizializamos
+    const mensaje = "Hola Mundo";
+    //2. Estimulo
+    
+    const mensaje2 = mensaje.trim();
+
+    //3. Observar el comportamiento esperado.
+
+    expect(mensaje).toBe(mensaje2);
+  });
+});
